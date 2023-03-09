dd9fc4c98414c2d626d036edb8b3b68ae5856d49
Dabney Comptrollers <comptrollers@dabney.caltech.edu>
2023-03-09 03:22:26 -0800

    yoink models and test cam

--- src/pose/test.py
+++ src/pose/test.py
@@ -3,7 +3,39 @@ from headpose.detect import PoseEstimator
 
 est = PoseEstimator()  #load the model
 # take an image using the webcam (alternatively, you could load an image)
-cam = cv2.VideoCapture(1)
+
+# define a video capture object
+vid = cv2.VideoCapture(0)
+
+while True:
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+    # frame = cv2.resize(frame, (224, 224))
+
+
+    cv2.waitKey(0)
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("frame", image)
+    est.detect_landmarks(image, plot=True)
+    # Display the resulting frame
+    print("a")
+
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
+
+
+exit()
+
+cam = cv2.VideoCapture()
 for i in range(cv2.CAP_PROP_FRAME_COUNT):
     cam.grab()
 ret, image = cam.retrieve()
